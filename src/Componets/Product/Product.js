074c415a10fd833bc1bea5fd0c7050e2f6d3bdb0
Your Name <you@example.com>
2021-10-20 04:39:16 +0800

    Final amazone commit

--- src/Componets/Product/Product.js
+++ src/Componets/Product/Product.js
@@ -3,10 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 import './Product.css';
+import Rating from 'react-rating';
 
 const Product = (props) => {
     // console.log(props);
-    const {name, img, seller, price, stock} = props.product;
+    const {name, img, seller, price, stock, star } = props.product;
    
 
     return (
@@ -19,6 +20,13 @@ const Product = (props) => {
                 <p><small>By: {seller} </small></p>
                 <p>price: {price} </p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+
+                <Rating   
+                 initialRating={star}
+                emptySymbol="fa fa-star-o fa-2x icon-g"
+                fullSymbol="fa fa-star fa-2x icon-g"
+                readonly></Rating>
+                <br />
                 <button
                     onClick={() => props.handleAddToCart(props.product)} 
                 className="purchase-btn"
